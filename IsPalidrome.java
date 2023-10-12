598c18d2eb20732527dd62a84f70684cfc3c7dc4
Vaishnav-123-bit <121089472+Vaishnav-123-bit@users.noreply.github.com>
2023-10-12 22:59:39 +0530

    Add files via upload

---
+++ IsPalidrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x==0){
+            return true;
+        
+        }
+        if(x<0||x%10==0){
+            return false;
+        }
+        int temp=x;
+        int rev=0;
+        while(temp!=0){
+            int digit=temp%10;
+            temp=temp/10;
+            rev=(rev*10)+digit;
+        }
+        if(rev==x){
+            return true;
+        }else{
+            return false;
+        }      
+    }
+}
