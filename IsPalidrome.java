7ce6e03f0653afdbd321137090b3b17f71cb5e9c
Vaishnav-mahadik <121089472+Vaishnav-123-bit@users.noreply.github.com>
2024-01-16 22:44:53 +0530

    Update IsPalidrome.java

--- IsPalidrome.java
+++ IsPalidrome.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x==0){
-            return true;
-            
+            return true;    
     
         }
         if(x<0||x%10==0){
